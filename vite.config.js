b427e2735f31adad800d9886ae6a739310adac93
sonsoleslp <poynting.slp@gmail.com>
2024-10-08 15:20:19 +0300

    Fist commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"./"
+  base:"./",
+  build: {
+    assetsInlineLimit: 0,  // Forces Vite to output assets as files instead of inlining them
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',  // Avoids hash in file names, making it easier to manage local files
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      },
+    },
+  }
 })
